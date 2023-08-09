315e9058051b9121105d606dc9c16186407442eb
Bruno Canto <bruno_scanto@hotmail.com>
2023-08-09 18:06:17 -0300

    Método salvar controller

--- AppListaCurso/app/src/main/java/devandroid/bruno/applistacurso/Controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/bruno/applistacurso/Controller/PessoaController.java
@@ -1,4 +1,26 @@
 package devandroid.bruno.applistacurso.Controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.bruno.applistacurso.model.Pessoa;
+
 public class PessoaController {
+
+
+    @NonNull
+    @Override
+    public String toString() {
+
+        Log.d("MVC_Controller", "Controller iniciada...");
+
+
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.i("MVC_Controller", "Salvo: " + pessoa.toString());
+
+    }
 }
